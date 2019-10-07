129f84244c489ead8bf08d347d578cecda80092d
charantej224 <charantej.career@gmail.com>
2019-10-07 14:52:32 -0500

    fix problem1

--- apps/main.py
+++ apps/main.py
@@ -4,7 +4,7 @@ from structure_conversion.tuple_dictionary import sort_tuple
 def run_apps():
     print("welcome to Lab1")
     string_op = StringOperations()
-    string_op.longest_substring('abcdefghijabcdefghij')
+    string_op.longest_substring('ababcdxa')
     sort_tuple()
 
 
--- string_apps/string_operation.py
+++ string_apps/string_operation.py
@@ -8,17 +8,28 @@ Sample Output: abcdx
 
 class StringOperations:
     def longest_substring(self, input_string):
+        temp_string = ""
+        longest_substring = ""
         char_list = []
-        temp_string = ''
-        longest_substring = ''
         for a in input_string:
             if a in char_list:
-                char_list.clear()
-                if len(longest_substring) < len(temp_string):
-                    longest_substring = temp_string
-                temp_string = ''
+                longest_substring = self.clean_list(char_list, a, temp_string, longest_substring)
+                char_list.append(a)
             if a not in char_list:
                 char_list.append(a)
-                temp_string += a
+
+        for a in char_list:
+            temp_string += a
+        if len(longest_substring) < len(temp_string):
+            longest_substring = temp_string
 
         print(longest_substring)
+
+    def clean_list(self, input_list, char, temp_string, longest_substring):
+        for a in input_list:
+            temp_string += a
+        for i in range(input_list.index(char) + 1):
+            del input_list[0]
+        if len(longest_substring) < len(temp_string):
+            longest_substring = temp_string
+        return longest_substring
